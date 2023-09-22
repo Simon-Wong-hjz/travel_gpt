141f33bf70095268b724f32519f3695d4f76f149
Simon Jingzhe Huang <v-simhuang@expediagroup.com>
2023-09-22 15:55:22 +0800

    fix CORS

--- backend/src/main/java/com/centific/travelgpt/backend/controller/TravelGptController.java
+++ backend/src/main/java/com/centific/travelgpt/backend/controller/TravelGptController.java
@@ -4,10 +4,7 @@ import com.centific.travelgpt.backend.entity.TravelGptRequest;
 import com.centific.travelgpt.backend.service.TravelGptService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 /**
@@ -19,6 +16,7 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin(value = "*")
 public class TravelGptController {
 
     private final TravelGptService travelGptService;
