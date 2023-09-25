3094d4269091eea3d92cc941103a3b12fccff394
Jojo Su Chen <v-jojochen@expediagroup.com>
2023-09-25 13:00:06 +0800

    feat: Add a  survey questionnaire link and back button in plan page

--- webapp/src/Page/PlanPage/index.js
+++ webapp/src/Page/PlanPage/index.js
@@ -1,13 +1,24 @@
 import './index.css';
+import { useNavigate } from 'react-router-dom'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
-
+import {
+  Button,
+} from 'antd';
 function Plan () {
+	const navigate = useNavigate();
 	const data = localStorage.getItem('data');
+
+	const backHome = ()=> {
+		navigate('/home');
+	}
 	// console.log('data', data);
 	return (
-		data && <div className="plan"> 
+		data && <div className="plan">
+			<h3>为了提高用户体验和产品迭代更新，诚挚邀请您填写下方的调查问卷, 非常感谢你的参与~</h3> 
+			<a href="https://www.wjx.cn/vm/mWPzZcY.aspx#" target="_blank" className='feedback_link'>使用体验反馈调查问卷</a>
 			<ReactMarkdown className='plan-content' children={data} remarkPlugins={[remarkGfm]} />
+			<Button type="primary" className='back_home' onClick={backHome}>返回首页</Button>
 		</div>
   );
 };
